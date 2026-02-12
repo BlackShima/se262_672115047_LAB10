c238b10286568dcfdd072f7581525bbf110d4b79
BlackShima <Blackshima02@gmail.com>
2026-02-12 10:27:38 +0700

    change api

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -19,33 +19,32 @@ app.post("/", function(req, res) {
             const data = response.data;
             const temp = data.main.temp;
             const desc = data.weather[0].description;
+            const lat = data.coord.lat;
+            const lon = data.coord.lon;
+            const icon = data.weather[0].icon;
+
             res.setHeader("Content-Type", "text/html");
             res.write("<h3>The temperature in " + cityName + " is " + temp + " degree Celcius.</h3>");
             res.write("<p>The weather currently is " + desc + "</p>");
+            res.write("<img src='https://openweathermap.org/img/wn/" + icon + "@2x.png'>");
             
+            const pollutionUrl = "https://api.openweathermap.org/data/2.5/air_pollution?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+            return axios.get(pollutionUrl);
+        })
+        .then(pollutionResponse => {
+            const pollutionData = pollutionResponse.data.list[0];
+            const aqi = pollutionData.main.aqi;
+            const pm10 = pollutionData.components.pm10;
+            const pm2_5 = pollutionData.components.pm2_5;
+            res.write("<p>The Air Quality Index is " + aqi + "</p>");
+            res.write("<p>The PM10:" + pm10 + " µg/m³</p>");
+            res.write("<p>The PM2.5:" + pm2_5 + " µg/m³</p>");
+            res.end();
         })
         .catch(err => {
             console.log(err);
         });
 });
-
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  
-  res.write('<!DOCTYPE html>\n');
-  res.write('<html>\n');
-  res.write('<head><title>Node.js Response Example</title></head>\n');
-  res.write('<body>\n');
-  res.write('<h1>Hello from Node.js!</h1>\n');
-  res.write('<p>This response was sent using the ServerResponse object.</p>\n');
-  res.write('</body>\n');
-  res.write('</html>');
-  
-  res.end();
-});
-
 app.listen(3000, function() {
     console.log("Server is running on port 3000.");
 });
